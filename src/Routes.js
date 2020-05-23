2bbf812301cf2589dfbb693b57b2acce41309f44
wesleyleandro7g <wesleyleandro.dev@gmail.com>
2020-05-23 01:40:33 -0300

    CRUD in ProjectController

--- src/Routes.js
+++ src/Routes.js
@@ -8,14 +8,21 @@ const UserController = require("./controllers/UserController");
 const Authentication = require("./controllers/Authentication");
 const ProjectController = require("./controllers/ProjectController");
 
+//### Rotas de cadastro e autenticação
 app.post("/user/register", UserController.create);
 app.get("/authentication", Authentication.authenticate);
 
+//### Midlleware de autenticação com o token JWT
 app.use(authMidlleware);
 
+//### Rotas para operações com usuários
 app.get("/user", UserController.list);
+
+//### Rotas para operações com projetos
 app.get("/project", ProjectController.listAll);
-app.get("/project/:_id", ProjectController.show);
+app.get("/project/:projectId", ProjectController.show);
+app.put("/project/:projectId", ProjectController.update);
+app.delete("/project/:projectId", ProjectController.delete);
 app.post("/project", ProjectController.create);
 
 module.exports = app;
--- src/controllers/ProjectController.js
+++ src/controllers/ProjectController.js
@@ -1,29 +1,81 @@
 const Project = require("../models/Project");
+const Task = require("../models/Task");
 
 module.exports = {
+  //### Controller de criação de projeto
   async create(req, res) {
+    const { title, description, tasks } = req.body;
+
     try {
-      const project = await Project.create({ ...req.body, user: req.userId });
+      const project = await Project.create({
+        title,
+        description,
+        user: req.userId,
+      });
+
+      //   await Promise.all(
+      //     tasks.map(async (task) => {
+      //       const projectTask = new Task({ ...task, project: project._id });
+
+      //       await projectTask.save();
+
+      //       project.task.push(projectTask);
+      //     })
+      //   );
+
+      //   await project.save();
 
       return res.send({ project });
     } catch (error) {
+      console.log(error);
       return res.send({ error });
     }
   },
+  //### Controller de listagem de todos todos os projetos de um usário
   async listAll(req, res) {
     try {
-      const project = await Project.find();
+      const project = await Project.find().populate("user");
 
       return res.send({ project });
     } catch (error) {
       return res.send({ error });
     }
   },
+  //### Controller de listagem de um projeto específico de um usuário
   async show(req, res) {
     try {
-      const project = await Project.findById(req.headers._id);
+      const project = await Project.findById(req.params.projectId);
 
       return res.send({ project });
+    } catch (error) {
+      return res.send({ error });
+    }
+  },
+  //### Controller de alteração de projetos
+  async update(req, res) {
+    const { title, description } = req.body;
+
+    try {
+      const project = await Project.findByIdAndUpdate(
+        req.params.projectId,
+        {
+          title,
+          description,
+        },
+        { new: true }
+      );
+
+      return res.status(200).send({ project });
+    } catch (error) {
+      return res.send({ error });
+    }
+  },
+  //### Controller de remoção de projetos
+  async delete(req, res) {
+    try {
+      await Project.findByIdAndRemove(req.params.projectId);
+
+      return res.status(200).send("Project removed");
     } catch (error) {
       return res.send({ error });
     }
