e594a43aa627799d99853ba2f035c936e43c2ee6
wesleyleandro7g <wesleyleandro.dev@gmail.com>
2020-05-22 23:08:45 -0300

    Add models Project and Task

---
+++ src/models/Task.js
@@ -0,0 +1,26 @@
+const mongoose = require("../database");
+
+const TaskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  project: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Project",
+    required: true,
+  },
+  assignedTo: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Task = mongoose.model("Task", TaskSchema);
+
+module.exports = Task;
